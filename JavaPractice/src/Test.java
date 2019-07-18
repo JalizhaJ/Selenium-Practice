5ec42e5472a6de31b5824f32ec84624ba17d0bb1
JalizhaJ <jalizha@gmail.com>
2019-07-18 18:21:04 +0530

    New change

---
+++ JavaPractice/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		int num = 5;
+		System.out.println(num);
+		num = 7;
+		System.out.println(num);
+		num = num +3;
+		System.out.println(num);
+
+	}
+
+}
